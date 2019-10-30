1f40acb8a693e910cdc6cb417e9acc8547702dab
DiovaneLuisPacheti <frontendestudo@gmail.com>
2019-10-30 02:57:36 -0300

    novo layout html retirando codigos e comentando

--- js/jogo.js
+++ js/jogo.js
@@ -12,19 +12,19 @@ var vezJogada = 1; // variavel que ira verificar a vez dos jogadores
 
 // craindo as referencias aos elementos da pagina do tabulario
 var outq1 = document.getElementById("outq1");//capturando id do quadro 1
-var outq2 = document.getElementById("outq2");//capturando id do quadro 
-var outq3 = document.getElementById("outq3");//capturando id do quadro 
-var outq4 = document.getElementById("outq4");//capturando id do quadro 
-var outq5 = document.getElementById("outq5");//capturando id do quadro 
-var outq6 = document.getElementById("outq6");//capturando id do quadro 
-var outq7 = document.getElementById("outq7");//capturando id do quadro 
-var outq8 = document.getElementById("outq8");//capturando id do quadro 
-var outq9 = document.getElementById("outq9");//capturando id do quadro 
-var sel1 = document.getElementById("sel1");
-var sel2 = document.getElementById("sel2");
-var vitoria1 = 0;
+var outq2 = document.getElementById("outq2");//capturando id do quadro 2
+var outq3 = document.getElementById("outq3");//capturando id do quadro 3
+var outq4 = document.getElementById("outq4");//capturando id do quadro 4
+var outq5 = document.getElementById("outq5");//capturando id do quadro 5
+var outq6 = document.getElementById("outq6");//capturando id do quadro 6
+var outq7 = document.getElementById("outq7");//capturando id do quadro 7
+var outq8 = document.getElementById("outq8");//capturando id do quadro 8
+var outq9 = document.getElementById("outq9");//capturando id do quadro 9
+var sel1 = document.getElementById("sel1"); //capturando id do campo do placar do player1
+var sel2 = document.getElementById("sel2"); //capturando id do campo do placar do player2
+var vitoria1 = 0;// variavel que vai receber o valor da vitoria e somar ao que vier do localStorage.getItem(vitoriaPlayer1)
 var vitoria2 = 0;
-var vitoriaPlayer1 = 0;
+var vitoriaPlayer1 = 0; 
 var vitoriaPlayer2 = 0;
 
 
@@ -40,10 +40,9 @@ outq7.addEventListener('click', function () { escolherPosicaoTabuleiro(2, 0, 7) 
 outq8.addEventListener('click', function () { escolherPosicaoTabuleiro(2, 1, 8) });
 outq9.addEventListener('click', function () { escolherPosicaoTabuleiro(2, 2, 9) });
 
+//function recebe posição do tabuleiro qual sera setada 
 function escolherPosicaoTabuleiro(linha, coluna, index) {
     var icones = document.createElement('i');//criando um elemento "i" 
-
-
     if (vezJogada % 2 != 0) {//jogador 1 - sempre que a variavel vezjogada tiver valor impar
         if (tabuleiro[linha][coluna] == "" || tabuleiro[linha][coluna] == undefined) {//verifica se o tabuleiro esta vazio nesta posição
 
@@ -89,17 +88,14 @@ function escolherPosicaoTabuleiro(linha, coluna, index) {
             }//fim do switch
             
         }else if(tabuleiro[linha][coluna] == "X" || tabuleiro[linha][coluna] == "O"){
-            icones.className = "text-danger"
-            var q = "sai"+index;
             
-            q.appendChild(icones);
-            vezJogada--;
+            vezJogada--; //decrementa o valor da jogada para ser o mesmo jogador jogando enquanto nao selecionar um quadro vazio
         }
-    } else {
-        if (tabuleiro[linha][coluna] == "" || tabuleiro[linha][coluna] == undefined) {
+    } else {// senao valor de vezjogada é par vez do jogador 2
+        if (tabuleiro[linha][coluna] == "" || tabuleiro[linha][coluna] == undefined) {//verificando se o vetor esta vazio
 
             tabuleiro[linha][coluna] = "O";
-            switch (index) {
+            switch (index) {//verificando qual caso escolher 
                 case 1:
                     //  outq1.textContent = "0";
                     icones.className = "far fa-circle"
@@ -143,10 +139,8 @@ function escolherPosicaoTabuleiro(linha, coluna, index) {
             vezJogada--;
         }
     }
-    //alert(tabuleiro[linha][coluna]);
-    //alert(tabuleiro)
-    vezJogada++;
-    verificarFimDeJogo();
+    vezJogada++; //incrementa a jogaga  e passa para a proxima vez
+    verificarFimDeJogo();// chama a function que vai verificar apos cada jogada se algum jogador venceu
 }//fim da function escolher Posição Tabuleiro
 
 function verificarFimDeJogo() {
@@ -162,21 +156,15 @@ function verificarFimDeJogo() {
         
             alert("Parabéns, jogador 1 ganhou!");
         
-            vitoria1++;
-            var v1 = parseInt(localStorage.getItem("vitoriaPlayer1"));
-            if(v1 > 0 ){
-                if(v1 == NaN){
+            vitoria1++;//se jogador 1 vencer vitorial1 que possuia valor zero incrementa 1 , mas  e zerada toda vez que ha um vencedor
+            var v1 = parseInt(localStorage.getItem("vitoriaPlayer1"));//capturando valor existente na variavel vitoriaPlayer1 e passando para tipo int
+            if(v1 > 0 ){// se valor de v1 for maior que 0
                  // salva os dados em localStorage
-                }    
                  localStorage.setItem("vitoriaPlayer1", vitoria1 + v1);
-               
-                //localStorage.setItem("vitoriaPlayer2", vitoria2);
-        
             }else{
-               // vitoriaPlayer1 = localStorage.getItem("vitoriaPlayer1" + vitoria1);
                localStorage.setItem("vitoriaPlayer1", vitoria1);
             }
-                limpaTabuleiro();
+            limpaTabuleiro();//chama function que limpa a pagina 
     } else if ((tabuleiro[0][0] == 'O' && tabuleiro[0][1] == 'O' && tabuleiro[0][2] == 'O') ||  // linha 1
         (tabuleiro[1][0] == 'O' && tabuleiro[1][1] == 'O' && tabuleiro[1][2] == 'O') ||  // linha 2
         (tabuleiro[2][0] == 'O' && tabuleiro[2][1] == 'O' && tabuleiro[2][2] == 'O') ||  // linha 3
@@ -191,20 +179,11 @@ function verificarFimDeJogo() {
         vitoria2++;
         var v2 = parseInt(localStorage.getItem("vitoriaPlayer2"));
         if(v2 > 0 ){
-            if(v2 == NaN){
-             // salva os dados em localStorage
-            }    
              localStorage.setItem("vitoriaPlayer2", vitoria2 + v2);
-           
-            //localStorage.setItem("vitoriaPlayer2", vitoria2);
-    
         }else{
-           // vitoriaPlayer1 = localStorage.getItem("vitoriaPlayer1" + vitoria1);
            localStorage.setItem("vitoriaPlayer2", vitoria2);
-           
-    
         }
-            limpaTabuleiro();
+        limpaTabuleiro();
         } else if (vezJogada > 9) {// senão se vez de jgaada passar de 9 jogadas empate
         alert("Ninguém ganhou essa partida");
         limpaTabuleiro();
